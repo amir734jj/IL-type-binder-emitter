34e57d241fb9278555622bd1e17c93ff3cbda7df
Amir <hesamian@uwm.edu>
2019-08-07 22:33:01 -0500

    minor

---
+++ IL-type-binder-emitter/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using IL_type_binder_emitter;
+
+namespace IL_type_binder_emitter
+{
+    public class Entity
+    {
+        public string Name { get; set; }
+    }
+
+    public interface ICommon
+    {
+        string Name { get; set; }
+    }
+
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            var table = new Dictionary<string, (Type, string)>
+            {
+                ["Name"] = (typeof(string), "Name")
+            };
+
+            var type = new CustomTypeGenerator<Entity, ICommon>(table).EmittedType;
+
+            var entity = new Entity {Name = "Amir "};
+
+            var rslt = (ICommon)Activator.CreateInstance(type, entity);
+
+            try
+            {
+                Console.WriteLine(rslt.Name);
+
+                Console.WriteLine("Hello World!");
+            } catch(Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
